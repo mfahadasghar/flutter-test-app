3c9baa81b658745de53fbcdd873af8b0e66b58fc
Fahad Asghar <fahadasghar360@gmail.com>
2025-09-13 18:09:13 +0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyBusinessCard());
+}
+
+class MyBusinessCard extends StatelessWidget {
+  const MyBusinessCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(
+                  'assets/profile.jpg',
+                ), // Add your picture here
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Fahad',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.tealAccent,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Card(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 25,
+                  vertical: 10,
+                ),
+                child: ListTile(
+                  leading: const Icon(Icons.phone, color: Colors.teal),
+                  title: const Text('+92 300 1234567'),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 25,
+                  vertical: 10,
+                ),
+                child: ListTile(
+                  leading: const Icon(Icons.email, color: Colors.teal),
+                  title: const Text('your.email@example.com'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
